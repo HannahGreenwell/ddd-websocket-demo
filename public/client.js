10fa6448ad470b5f491514c9c710416e4884a5e3
Hannah Greenwell <hannah.greenwell@gmail.com>
2019-09-20 09:21:20 +1000

    stage2 changes

--- public/client.js
+++ public/client.js
@@ -1,3 +1,32 @@
 (function() {
 
+  // HTML elements
+  const form = document.querySelector('form');
+  const nameField = document.querySelector('input#name');
+  const messageField = document.querySelector('input#message');
+  const messageList = document.querySelector('ul');
+
+  // Construct a WebSocket object that will create and
+  // manage the connection to the WebSocket server
+  const websocket = new WebSocket('ws://localhost:3000');
+
+  // HTML events
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const author = nameField.value;
+    const content = messageField.value;
+    const message = {
+      author,
+      content
+    };
+    console.log('Message', message);
+    messageField.value = '';
+    return false;
+  });
+
+  // WebSocket events
+  websocket.addEventListener('open', function(event) {
+    console.log('Connected to the WebSocket server');
+  });
+
 }());
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 // Require dependencies
 const express = require('express');
 const http = require('http');
+const WS = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
 // Create an express app
 const app = express();
 
+// Specify 'public' as the root directory from which to serve static assets
+app.use(express.static('public'));
+
 // Create a HTTP server
 const httpServer = http.createServer(app);
+
+// Create a WebSocket server and integrate it with our HTTP server
+const websocketServer = new WS.Server({ server: httpServer });
+
+// WebSocket events
+websocketServer.on('connection', function(websocket) {
+  console.log('A user connected');
+
+  websocket.on('close', function(code) {
+    console.log('User disconnected');
+    console.log('Close code: ', code);
+  });
+});
 
 // Start the server
 httpServer.listen(PORT, function() {
   console.log(`Listening on ${PORT}`);
-});
-
-// Declare routes
-app.get('/hello-world', function(req, res) {
-  res.send('<h1>Hello World!</h1>');
 });
