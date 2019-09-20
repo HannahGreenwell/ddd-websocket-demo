c59bfaeedb374d7be4c804745ec73da305e452e9
Hannah Greenwell <hannah.greenwell@gmail.com>
2019-09-21 05:55:47 +1000

    Changed to arrow functions

--- server.js
+++ server.js
@@ -7,15 +7,11 @@ const PORT = process.env.PORT || 3000;
 // Create an express app
 const app = express();
 
+// Specify ‘public’ as the root directory from which to serve static assets
+app.use(express.static('public'));
+
 // Create a HTTP server
 const httpServer = http.createServer(app);
 
 // Start the server
-httpServer.listen(PORT, function() {
-  console.log(`Listening on ${PORT}`);
-});
-
-// Declare routes
-app.get('/hello-world', function(req, res) {
-  res.send('<h1>Hello World!</h1>');
-});
+httpServer.listen(PORT, () => console.log(`Listening on ${PORT}`));
